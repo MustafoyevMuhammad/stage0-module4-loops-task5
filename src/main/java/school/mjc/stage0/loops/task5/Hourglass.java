ff670dd67477dd4f4c4095d35ba4750eeedc937f
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2022-08-27 10:18:20 +0500

    Update Hourglass.java

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,49 +2,21 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-        boolean ok = false;
-        for (int i = 0; i < height/2; i++) {
-            for (int j = 0; j < height; j++) {
-                if(j == i) {
-                    System.out.print(8);
-                    ok =true;
+      for (int i = 0; i < height; i++) {//0
+            if (i<height/2) {
+                for (int j = 0; j < height; j++) {//0
+                    if (j >= i && j <= height - (i + 1))
+                        System.out.print(8);
+                    else
+                        System.out.print(" ");
                 }
-                else if(j + i == height - 1){
-                    System.out.print(8);
-                    ok = false;
+            } else{
+                for (int j = height; j >0; j--) {//3   0-5
+                    if (j > height - (i + 1) && j <= i+1)
+                        System.out.print(8);
+                    else
+                        System.out.print(" ");
                 }
-                else if(ok){
-                    System.out.print(8);
-                }
-                else
-                    System.out.print(" ");
-
-            }
-            System.out.println();
-        }
-        if (height % 2 != 0) {
-            for (int i = 0; i < height/2; i++) {
-                System.out.print(" ");
-            }
-            System.out.println(8);
-        }
-        ok = false;
-        for (int i = height/2 + 1; i < height; i++) {
-            for (int j = 0; j < height; j++) {
-                if(j + i == height - 1){
-                    System.out.print(8);
-                    ok = true;
-                }
-                else if(j == i) {
-                    System.out.print(8);
-                    ok =false;
-                }
-                else if(ok){
-                    System.out.print(8);
-                }
-                else
-                    System.out.print(" ");
-
             }
             System.out.println();
         }
